d11813d144510b2773d5f33af830ad31f07fbcad
navinmanohar <navinmanohar78086@gmail.com>
2024-09-30 11:29:47 +0530

    add get

--- server/index.js
+++ server/index.js
@@ -52,7 +52,9 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal Server Error' });
 });
-
+app.get('/',(req,re)=>{
+  res.send("hello")
+})
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
